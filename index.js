1500d19618c3c327856400c76ec3f2ee19f5ea28
Danish <dkhans2001@gmail.com>
2025-05-17 20:32:58 -0500

    nested

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
+  let locked = false;
+
+  return (passwordAttempt) => {
+    if (locked) {
+      return "Account locked due to too many failed login attempts";
+    }
+
+    attemptCount++;
+
+    if (passwordAttempt === userInfo.password) {
+      return "Login successful";
+    } else if (attemptCount >= 3) {
+      locked = true;
+      return `Attempt 3: Login failed`;
+    } else {
+      return `Attempt ${attemptCount}: Login failed`;
+    }
+  };
+}
+
+module.exports = {
+  createLoginTracker,
+};
+
 
 
 
